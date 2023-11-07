9ac1ddc738504148e6db4eee691e0daa04878da3
juharing <julius.haring@dhl.com>
2023-11-07 12:28:27 +0000

    feat: add q value plotting

--- agents/q_learning.py
+++ agents/q_learning.py
@@ -1,6 +1,10 @@
+import sys
+import json
 from itertools import product
+from matplotlib import cm
 
 import numpy as np
+import matplotlib.pyplot as plt
 from agents.base import RLAgent
 from environment.base import Environment
 from environment.types import actions_dict, states_dict, action_space
@@ -28,17 +32,19 @@ class QLearningAgent(RLAgent):
         can take on a known fixed number of discrete values.
         """
         # Flatten the state into a list of values
-        flattened_state = []
-        for key in sorted(state.keys()):
-            flattened_state.extend(state[key][0])  # Position tuple
-            flattened_state.extend(state[key][1])  # Velocity tuple
+        # flattened_state = []
+        # for key in sorted(state.keys()):
+        #     flattened_state.extend(state[key][0])  # Position tuple
+        #     flattened_state.extend(state[key][1])  # Velocity tuple
 
-        # Convert list of state values into a string to use as a dictionary key
-        state_str = "_".join(map(str, flattened_state))
+        # # Convert list of state values into a string to use as a dictionary key
+        # state_str = "_".join(map(str, flattened_state))
+
+        state_str = json.dumps(state)
 
         # Ensure there is an entry for this state string in the Q-table
         if state_str not in self.q_table:
-            self.q_table[state_str] = [0] * len(self.get_action_space())
+            self.q_table[state_str] = [-sys.maxsize] * len(self.get_action_space())
 
         return state_str
 
@@ -130,6 +136,47 @@ class QLearningAgent(RLAgent):
             self.evaluate_best_env(reward=reward, environment=self.env)
             print(f"Episode: {episode}, Total reward: {total_reward}")
         self.render_best()
+        self.render_q_table()
+
+    def render_q_table(self):
+        fig = plt.figure(figsize=(8, 6))
+        ax = fig.add_subplot(111, projection="3d")
+
+        # Create a list to collect all Q-values for normalization
+        all_q_values = [
+            action for actions in self.q_table.values() for action in actions
+        ]
+
+        # Normalize the Q-values between 0 and 1
+        max_q_value = max(all_q_values)
+        min_q_value = min(all_q_values)
+        norm = plt.Normalize(min_q_value, max_q_value)
+
+        # Create a scalar mappable for the color bar with the normalization
+        scalar_map = cm.ScalarMappable(norm=norm, cmap="jet")
+
+        agent_name = next(obj for obj in self.env.objects if obj.is_movable).name
+
+        for state_str, actions in self.q_table.items():
+            state = json.loads(state_str)[agent_name]
+            max_q_value = max(actions)
+
+            # Get the corresponding color for this Q-value
+            color = scalar_map.to_rgba(max_q_value)
+
+            # Plot the state with its corresponding Q-value color
+            ax.scatter(state[0][0], state[0][1], state[0][2], color=color, marker="o")
+
+        # Set labels for axes
+        ax.set_xlabel("State Dimension 1")
+        ax.set_ylabel("State Dimension 2")
+        ax.set_zlabel("State Dimension 3")
+
+        # Add the color bar using the scalar mappable
+        cbar = fig.colorbar(scalar_map, shrink=0.5, aspect=5, ax=ax)
+        cbar.set_label("Q-value")
+
+        plt.show()
 
     def learn_from_batch(self, batch):
         for state, action, reward, next_state, done in batch:
